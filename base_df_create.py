142652bfe8bfaa6ba2e3a0e2d3c2cbe94ae9c631
manoj.k.rangarajan <manoj83.r@gmail.com>
2020-01-21 20:40:09 +0530

    Commit New change

--- base_df_create.py
+++ base_df_create.py
@@ -1,17 +1,48 @@
-import logging
-
+from pyspark.sql.types import *
+from schema_file import SchemaFile as sf
+from pyspark.sql.functions import regexp_replace, unix_timestamp, from_unixtime
 
 class CreateDF:
     def __init__(self):
         pass
 
-    def dataFrameCreate(self, spark, directory=None, file_name=None, file_format=None,
-                        seperator=",", headerAvailable=False, schemaName=None):
-        spark_logger = logging.Log4j(spark)
+    @staticmethod
+    def data_frame_create(spark, directory=None, file_name=None, file_format=None,
+                          sep=",", headerAvailable=False, schemaName=None):
         try:
-            data_frame = spark.read.load(file_name, format=file_format, sep=seperator,
+            data_frame = spark.read.load(file_name, format=file_format, sep=sep,
                                          header=headerAvailable, schema=schemaName)
-            spark_logger.info("Data Frame create successfully")
+            print("Data Frame create successfully for:",file_name)
             return data_frame
         except:
-            spark_logger.info("Problem in creating Data Frame")
+            print("Problem in creating Data Frame for:",file_name)
+
+    # UDF to Load Product data to product Data Frame
+    def product_df(self, spark):
+        product_tmp_df = self.data_frame_create(spark, None, "Dataset-Product.txt", "csv", "|", False, sf.productSchema)
+        product_df = product_tmp_df.withColumn("product_price", regexp_replace(product_tmp_df.product_price, '\$', ''). \
+                                            cast(IntegerType()))
+        return product_df
+
+    # Load Customer data to product Data Frame
+    def customer_df(self, spark):
+        customer_df = self.data_frame_create(spark, None, "Dataset-Customer.txt", "csv", "|", False, sf.customerSchema)
+        return customer_df
+
+    # Load Sales data to product Data Frame
+    def sales_df(self, spark):
+        sales_tmp_df = self.data_frame_create(spark, None, "Dataset-Sales.txt", "csv", "|", False, sf.salesSchema)
+        sales_df = sales_tmp_df.withColumn("total_amount", regexp_replace(sales_tmp_df.total_amount, "\$", "").
+                                    cast(IntegerType())).withColumn("timestamp",
+                                                                    from_unixtime(unix_timestamp(sales_tmp_df.timestamp,
+                                                                                                 'MM/dd/yy HH:mm:SS')).
+                                                                                                cast(TimestampType()))
+        return sales_df
+
+    def refund_df(self, spark):
+        refund_tmp_df = self.data_frame_create(spark, None, "Dataset-Refund.txt", "csv", "|", False, sf.refundSchema)
+        refund_df = refund_tmp_df.withColumn('timestamp', from_unixtime(unix_timestamp(refund_tmp_df.timestamp,
+                                                                                    "MM/dd/yy HH:mm:ss")).
+                                          cast(TimestampType())). \
+            withColumn('refund_amount', regexp_replace(refund_tmp_df.refund_amount, '\$', '').cast(IntegerType()))
+        return refund_df
--- customerDataAnalysis.py
+++ customerDataAnalysis.py
@@ -1,139 +1,51 @@
-import pysession
+from pysession import SparkSess
 from base_df_create import CreateDF
-from schema_file import SchemaFile as sf
-from pyspark.sql import functions as f
 from pyspark.sql.types import *
-from pyspark.sql.functions import regexp_replace, unix_timestamp, from_unixtime, year, month
+from pyspark.sql import functions as f
+from pyspark.sql.functions import year
+import dist_of_sales
+import top_customers
+import total_trans_without_refund
+import cust_details_most_purchase
+
 
 if __name__ == "__main__":
-    spark = pysession.SparkSess.sessionCreate("CustDataAnalysis", "local")
+    spark = SparkSess().session_create("CustDataAnalysis", "LOCAL")
 
+    createDF = CreateDF()
     # Load Product data to product Data Frame
-    product_tmpDF = CreateDF.dataFrameCreate("Dataset-Sales.txt", "csv", "|", False, sf.salesSchema)
-    productDF = product_tmpDF.withColumn("product_price",
-                                         regexp_replace(product_tmpDF.product_price, '\$', '').cast(IntegerType()))
+    productDF = createDF.product_df(spark)
     productDF.show(5)
-
     # Load Customer data to product Data Frame
-
-    customerDF = spark.read.load("Dataset-Customer.txt", format="csv", sep="|", header=False,
-                                 schema=sf.customerSchema)
+    customerDF = createDF.customer_df(spark)
     customerDF.show(5)
-
     # Load Sales data to product Data Frame
-    # salesDF = spark.read.load(spark, "Dataset-Sales.txt", "csv", "|", False, sf.salesSchema)
-
-    salesDF = spark.read.load("Dataset-Sales.txt", format="csv", sep="|", header=False, schema=sf.salesSchema)
-    salesDF = salesDF.withColumn("total_amount", regexp_replace(salesDF.total_amount, "\$", "").cast(IntegerType())). \
-        withColumn("timestamp",
-                   from_unixtime(unix_timestamp(salesDF.timestamp, 'MM/dd/yy HH:mm:SS')).cast(TimestampType()))
+    salesDF = createDF.sales_df(spark)
     salesDF.show(5)
-
     # Load Refund data to product Data Frame
-
-    refundDF = spark.read.load("Dataset-Refund.txt", format="csv", sep="|", header=False, schema=sf.refundSchema)
-    refundDF = refundDF.withColumn('timestamp',
-                                   from_unixtime(unix_timestamp(refundDF.timestamp, "MM/dd/yy HH:mm:ss")).cast(
-                                       TimestampType())). \
-        withColumn('refund_amount', regexp_replace(refundDF.refund_amount, '\$', '').cast(IntegerType()))
+    refundDF = createDF.refund_df(spark)
     refundDF.show(5)
 
-    distOfSales_DF = salesDF.join(productDF, salesDF.product_id == productDF.product_id, 'outer'). \
-        select(productDF.product_name, productDF.product_type, salesDF.product_id)
-    distOfSales_DF.printSchema()
-
     # Question2: Display the distribution of sales by product name and product type.
-    distOfSales_DF_count = distOfSales_DF.groupBy(distOfSales_DF.product_name, distOfSales_DF.product_type).count()
-    distOfSales_DF_count.coalesce(1).write.save("DistOfSales_count", format="csv", mode="overwrite",
-                                                header=True, sep=",")
-    print("Distribution of sales by product name and product type is stored "
-          "in the Directory DistOfSales_count in the csv format")
-
+    dist_of_sales.dist_of_sales(salesDF, productDF)
     # Question3: Get the Top 100 customers who are dosing the most of the transactions and store those
     # tables in hive table to give it to Downstream
-    saleByCustomerDF = salesDF.join(customerDF, salesDF.customer_id == customerDF.customer_id, "left"). \
-        select(salesDF.transaction_id, salesDF.customer_id, customerDF.customer_first_name,
-               customerDF.customer_last_name)
-    saleByCustomerDF.printSchema()
-    saleByCustomerDF.show(5)
-    transCountByCustDF = saleByCustomerDF. \
-        groupBy(saleByCustomerDF.customer_first_name, saleByCustomerDF.customer_last_name).count()
-    transCountByCustDF.printSchema()
-    transCountByCustDF.show(5)
-    tCountTop100CustDF = transCountByCustDF.orderBy("count", ascending=False).limit(100)
-    # Uncomment below line while running in cluster
-    # tCountTop100CustDF.write.saveAsTable("manoj_spark1.top_100_customer_transaction", mode="overwrite",
-    #                                   partitionBy=None)
-
-    # Question4:Calculate the total amount of all transactions that happened in year 2013
-    # and have not been refunded as of today.
-    sales2013DF = salesDF.where(year(salesDF.timestamp) == '2013')
-    # Join Sales table with Refund Table
-    sales2013NotRefunded = sales2013DF.join(refundDF, salesDF.transaction_id == refundDF.original_transaction_id,
-                                            "left")
-    # Get Not Refunded Transaction by checking trans id in refunded table is Null
-    sales2013NotRefunded = sales2013NotRefunded.where(sales2013NotRefunded.original_transaction_id.isNull())
-    totAmtSales2013NotRefunded = \
-        sales2013NotRefunded.select(sales2013NotRefunded.total_amount).groupBy().sum().collect()[0][0]
-    print("Total amount 2013 transactions that have not been refunded=" + str(totAmtSales2013NotRefunded))
-
+    top_customers.top_cust_trans(salesDF, customerDF, 100)
+    # Question4:Calculate the total amount of all transactions that happened in year 2013 and
+    # have not been refunded as of today.
+    total_trans_without_refund.total_trans_not_refunded(salesDF, refundDF, '2013')
     # Question5:Display the customer name who made the second most purchases in the month of May2013.
     # Refunds should be excluded.
-    salesMay2013DF = salesDF.where((year(salesDF.timestamp) == '2013') & (month(salesDF.timestamp) == '05'))
-    salesMay2013NotRefunded = salesMay2013DF.join(refundDF, salesDF.transaction_id == refundDF.original_transaction_id,
-                                                  "left").select(salesMay2013DF.transaction_id.alias("transaction_id1"),
-                                                                 salesMay2013DF.customer_id.alias("customer_id1"),
-                                                                 salesMay2013DF.product_id.alias("product_id1"),
-                                                                 salesMay2013DF.timestamp.alias("timestamp1"),
-                                                                 salesMay2013DF.total_amount.alias("total_amount1"),
-                                                                 salesMay2013DF.total_quantity.alias("total_quantity1"),
-                                                                 refundDF.refund_id.alias("refund_id2"),
-                                                                 refundDF.original_transaction_id.alias(
-                                                                     "original_transaction_id2"),
-                                                                 refundDF.customer_id.alias("customer_id2"),
-                                                                 refundDF.product_id.alias('product_id2'),
-                                                                 refundDF.timestamp.alias('timestamp2'),
-                                                                 refundDF.refund_amount.alias('refund_amount2'),
-                                                                 refundDF.refund_quantity.alias('refund_quantity2'))
-    salesMay2013NotRefunded = salesMay2013NotRefunded.where(salesMay2013NotRefunded.original_transaction_id2.isNull())
-    salesMay2013NotRefundedDF = salesMay2013NotRefunded.select(salesMay2013NotRefunded.transaction_id1,
-                                                               salesMay2013NotRefunded.customer_id1,
-                                                               (year(salesMay2013NotRefunded.timestamp1)).alias(
-                                                                   "col_year"))
-    custSalesMay2013DF = salesMay2013NotRefundedDF.join(customerDF,
-                                                        salesMay2013NotRefundedDF.customer_id1 == customerDF.
-                                                        customer_id, 'inner'). \
-        select(customerDF.customer_id,
-               customerDF.customer_first_name,
-               customerDF.customer_last_name,
-               salesMay2013NotRefundedDF.col_year,
-               salesMay2013NotRefundedDF.transaction_id1)
-    custSalesMay2013DF.show(5)
-    custSalesMay2013count_DF = custSalesMay2013DF.groupBy(customerDF.customer_id, customerDF.customer_first_name,
-                                                          customerDF.customer_last_name, custSalesMay2013DF.col_year). \
-        agg(f.count(salesMay2013NotRefundedDF.transaction_id1).alias("transaction_count"))
-    custSalesMay2013count_DF.show(5)
-    from pyspark.sql.window import Window
-    from pyspark.sql.functions import dense_rank
-
-    spec = Window.partitionBy(custSalesMay2013count_DF.col_year). \
-        orderBy(custSalesMay2013count_DF.transaction_count.desc())
-    custSalesMay2013CountRanked_DF = custSalesMay2013count_DF.withColumn('rnk', dense_rank().over(spec))
-    secondMostPurchaseMay2013DF = custSalesMay2013CountRanked_DF.where(custSalesMay2013CountRanked_DF.rnk == 2)
-    secondMostPurchaseMay2013DF.show(5)
-    # secondMostPurchaseMay2013DF.write.saveAsTable("manoj_spark1.second_most_purchase_may2013")
-
-    # Question6:Find a product that has not been sold at least once (if any).
+    cust_details_most_purchase.cust_with_n_rank_purchase(salesDF, refundDF, customerDF, 2, '05', '2013')
+    # Question6:Find a product that has not been sold at least once (if any). Get Distinct product id
     distinct_soldProdidDF = salesDF.select(salesDF.product_id.alias("sold_prod_id")).distinct()
-    distinct_soldProdidDF.show(5)
     prodSalesJoinDF = productDF.join(distinct_soldProdidDF,
                                      productDF.product_id == distinct_soldProdidDF.sold_prod_id, "left")
     prodNotSoldDF = prodSalesJoinDF.where(prodSalesJoinDF.sold_prod_id.isNull())
     prodNotSoldDF.show(5)
     # prodNotSoldDF.write.saveAsTable("manoj_spark1.product_not_sold", mode="overwrite", partitionBy=None)
 
-    # Question7:Calculate the total number of users who purchased the same product
-    # consecutively at least 2 times on a given day.
+    # Question7:Calculate the total number of users who purchased the same product at least 2 times on a given day.
     salesCustDateGrpDF = salesDF.groupBy(salesDF.customer_id, salesDF.timestamp.cast(DateType())). \
         agg(f.count(salesDF.transaction_id).alias("transCount"))
     numOfUserConsPur = salesCustDateGrpDF.where(salesCustDateGrpDF.transCount > 1).count()
